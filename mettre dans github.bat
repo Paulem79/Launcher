37fc1d003eb77182da8344d568734255fba7a3eb
BuildTools <paulemdu79@gmail.com>
2023-03-28 21:24:38 +0200

    Launcher v1.2

--- mettre dans github.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p version=Entrez la note de version : 
-git add .
-git commit -m "%version%"
-git remote add origin https://github.com/Paulem79/Launcher.git
-git push -u origin master
