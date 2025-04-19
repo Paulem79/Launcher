da6b977f8c686c19c1171d677f5f9b743374d0cc
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-04-19 14:53:47 +0200

    Update MinecraftInfos.java

--- src/main/java/ovh/paulem/launchermc/game/minecraft/MinecraftInfos.java
+++ src/main/java/ovh/paulem/launchermc/game/minecraft/MinecraftInfos.java
@@ -3,10 +3,10 @@ package ovh.paulem.launchermc.game.minecraft;
 import fr.flowarg.openlauncherlib.NoFramework;
 
 public class MinecraftInfos {
-    public static final String GAME_VERSION = "1.21";
+    public static final String GAME_VERSION = "1.21.5";
     //public static final ForgeVersionType FORGE_VERSION_TYPE = ForgeVersionType.NEW;
     public static final NoFramework.ModLoader MODLOADER = NoFramework.ModLoader.FABRIC;
-    public static final String MODLOADER_VERSION = "0.16.0";
+    public static final String MODLOADER_VERSION = "0.16.13";
 
-    public static final String MODS_LIST_URL = "https://raw.githubusercontent.com/Paulem79/Launcher/updater/mods_list.json";
+    public static final String MODS_LIST_URL = "https://raw.githubusercontent.com/Paulem79/Launcher/updater/mods.json";
 }
