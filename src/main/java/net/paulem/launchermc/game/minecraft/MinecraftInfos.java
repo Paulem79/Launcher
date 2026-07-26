04e8a935c0273e735d8106873e839eda42341548
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-07-26 16:32:39 +0200

    chore: edited minecraft infos

--- src/main/java/net/paulem/launchermc/game/minecraft/MinecraftInfos.java
+++ src/main/java/net/paulem/launchermc/game/minecraft/MinecraftInfos.java
@@ -2,21 +2,24 @@ package net.paulem.launchermc.game.minecraft;
 
 import fr.flowarg.flowupdater.utils.ModFileDeleter;
 import fr.flowarg.flowupdater.versions.ModLoaderVersionBuilder;
-import fr.flowarg.flowupdater.versions.fabric.FabricVersionBuilder;
+import fr.flowarg.flowupdater.versions.forge.ForgeVersionBuilder;
 import fr.flowarg.openlauncherlib.NoFramework;
 
 public class MinecraftInfos {
-    public static final String GAME_VERSION = "1.21.11";
-    //public static final ForgeVersionType FORGE_VERSION_TYPE = ForgeVersionType.NEW;
-    public static final NoFramework.ModLoader MODLOADER = NoFramework.ModLoader.FABRIC;
-    public static final String MODLOADER_VERSION = "0.18.4";
+    private MinecraftInfos() {
+        /* This utility class should not be instantiated */
+    }
+
+    public static final String GAME_VERSION = "1.20.1";
+    public static final NoFramework.ModLoader MODLOADER = NoFramework.ModLoader.FORGE;
+    public static final String MODLOADER_VERSION = "1.20.1-47.4.22";
 
     public static final String MODS_LIST_URL = "https://raw.githubusercontent.com/Paulem79/Launcher/updater/mods_potes.json";
     
-    public static final ModLoaderVersionBuilder<?, ?> GAME = new FabricVersionBuilder()
-            //.withFabricVersion(MinecraftInfos.MODLOADER_VERSION)
-            /*.withMods(MinecraftInfos.MODS_LIST_URL)
+    public static final ModLoaderVersionBuilder<?, ?> GAME = new ForgeVersionBuilder()
+            .withForgeVersion(MinecraftInfos.MODLOADER_VERSION)
+            .withMods(MinecraftInfos.MODS_LIST_URL)
             .withCurseMods(MinecraftInfos.MODS_LIST_URL)
-            .withModrinthMods(MinecraftInfos.MODS_LIST_URL)*/
+            .withModrinthMods(MinecraftInfos.MODS_LIST_URL)
             .withFileDeleter(new ModFileDeleter(true));
 }
