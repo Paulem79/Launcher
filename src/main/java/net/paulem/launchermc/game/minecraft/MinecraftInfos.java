abcffc8a8b052f65fa9bc0d78e19a9550c6e00a6
Paulem <66211574+Paulem79@users.noreply.github.com>
2026-09-20 20:07:45 +0200

    refactor: switched to fabric 26.2

--- src/main/java/net/paulem/launchermc/game/minecraft/MinecraftInfos.java
+++ src/main/java/net/paulem/launchermc/game/minecraft/MinecraftInfos.java
@@ -2,7 +2,7 @@ package net.paulem.launchermc.game.minecraft;
 
 import fr.flowarg.flowupdater.utils.ModFileDeleter;
 import fr.flowarg.flowupdater.versions.ModLoaderVersionBuilder;
-import fr.flowarg.flowupdater.versions.forge.ForgeVersionBuilder;
+import fr.flowarg.flowupdater.versions.fabric.FabricVersionBuilder;
 import fr.flowarg.openlauncherlib.NoFramework;
 
 public class MinecraftInfos {
@@ -11,14 +11,14 @@ public class MinecraftInfos {
         /* This utility class should not be instantiated */
     }
 
-    public static final String GAME_VERSION = "1.20.1";
-    public static final NoFramework.ModLoader MODLOADER = NoFramework.ModLoader.FORGE;
-    public static final String MODLOADER_VERSION = "1.20.1-47.4.22";
+    public static final String GAME_VERSION = "26.2";
+    public static final NoFramework.ModLoader MODLOADER = NoFramework.ModLoader.FABRIC;
+    public static final String MODLOADER_VERSION = "0.19.5";
 
     public static final String MODS_LIST_URL = "https://raw.githubusercontent.com/Paulem79/Launcher/updater/mods.json";
 
-    public static final ModLoaderVersionBuilder<?, ?> GAME = new ForgeVersionBuilder()
-            .withForgeVersion(MinecraftInfos.MODLOADER_VERSION)
+    public static final ModLoaderVersionBuilder<?, ?> GAME = new FabricVersionBuilder()
+            .withFabricVersion(MinecraftInfos.MODLOADER_VERSION)
             .withModrinthMods(MinecraftInfos.MODS_LIST_URL)
             .withFileDeleter(new ModFileDeleter(true));
 }
